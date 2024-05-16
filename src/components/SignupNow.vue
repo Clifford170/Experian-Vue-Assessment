9af2cc2de729a009fb7dbe4879e01393fcb1bea6
Clifford170 <cliffordbright107@gmail.com>
2024-05-16 09:29:50 +0200

    side and signup component

---
+++ src/components/SignupNow.vue
@@ -0,0 +1,36 @@
+<script setup></script>
+
+<template>
+    <section class="sign-up">
+        <div class="container-fluid pt-5">
+            <div class="row">
+                <div class="col-12 text-center sign-up-title">
+                    <h2>Sign Up Now and Take Your Business <br> to the <span><img
+                            src="/src/assets/Exprerian/DrivenWording – 1.png" alt=""></span> Level</h2>
+                </div>
+                <div class="col-12 text-center innovative">
+                    <p>Innovative, user-friendly information solutions that you can trust</p>
+                </div>
+            </div>
+        </div>
+    </section>
+</template>
+
+<style>
+    .sign-up {
+        min-height: 330px;
+        background: #ECF0F6;
+    }
+
+    .sign-up-title {
+        font-size: 50px;
+        font-weight: bold;
+        width: 654px;
+        color: #6D2077;
+    }
+
+    .innovative {
+        font: normal normal bold 18px/30px Roboto;
+        color: #000000;
+    }
+</style>
