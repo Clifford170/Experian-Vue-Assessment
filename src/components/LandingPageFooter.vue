c7eb890955b3c2a1697df803e5dc39b44d2a3beb
Clifford170 <cliffordbright107@gmail.com>
2024-05-16 09:26:01 +0200

    landing page footer component

---
+++ src/components/LandingPageFooter.vue
@@ -0,0 +1,47 @@
+
+<script setup></script>
+<template>
+    <section>
+        <div class="container-fluid">
+            <div class="row landing-footer d-flex align-items-center">
+                <div class="col">
+                    <img src="/src/assets/Exprerian/CompuloanLogo.png">
+                </div>
+                <div class="col">
+                    <img src="/src/assets/Exprerian/ColumbusLogo.png">
+                </div>
+                <div class="col">
+                    <img src="/src/assets/Exprerian/CodixLogo.png">
+                </div>
+                <div class="col">
+                    <img src="/src/assets/Exprerian/XceleratorLogo.png">
+                </div>
+                <div class="col">
+                    <img src="/src/assets/Exprerian/AASLogo.png">
+                </div>
+                <div class="col">
+                    <img src="/src/assets/Exprerian/IncomePredictorLogo.png">
+                </div>
+                <div class="col">
+                    <img src="/src/assets/Exprerian/BatchManagerLogo.png">
+                </div>
+                <div class="col">
+                    <img src="/src/assets/Exprerian/AVSLogo.png">
+                </div>
+            </div>
+        </div>
+    </section>
+</template>
+
+<style>
+    .landing-footer {
+        padding-left: 100px;
+        padding-right: 100px;
+        min-height: 141px;
+    }
+    @media screen and (max-width: 699px) {
+        .landing-footer {
+            padding: 15px;
+        }
+    }
+</style>
