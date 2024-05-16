30d251a64fcdf9f0b948fdf78c923296e64a18de
Clifford170 <cliffordbright107@gmail.com>
2024-05-16 09:28:41 +0200

    financial and table component

---
+++ src/components/FinancialInsights.vue
@@ -0,0 +1,66 @@
+<script setup></script>
+
+<template>
+    <section class="financial">
+        <div class="container-fluid align-content-end">
+            <div class="row">
+                <div class="col-6 col-sm-12 col-lg-6 col-xl-6 col-super-4">
+                    <img class="image" src="/src/assets/Exprerian/Infographic2.png">
+                </div>
+                <div class="col-6 col-sm-12 col-lg-6 col-xl-6 col-super-8 align-content-center">
+                    <h2 class="financial-title mb-5"><span>Financial <img src="/src/assets/Exprerian/InsightsWording.png"> in the <br>Palm of Your Hands </span></h2>
+                    <p class="financial-text">Drive intelligent decisions, new product innovation and better targeting through insight <br>
+                    and technology. Helping to deliver a frictionless digital customer experience.</p>
+                </div>
+            </div>
+        </div>
+    </section>
+</template>
+
+<style>
+    .financial {
+        background-image: url("/src/assets/Exprerian/BG2.png");
+        background-size: cover;
+        background-position: center center;
+        background-repeat: no-repeat;
+    }
+    .financial .container-fluid {
+        padding-left: 109px;
+        padding-right: 109px;
+        min-height: 600px;
+    }
+    .image {
+        height: 480px;
+    }
+    .financial .financial-title,
+    .financial .financial-text {
+        color: #ffffff;
+    }
+    .financial .financial-title {
+        font-size: 70px;
+    }
+    .financial .financial-text {
+        font-size: 18px;
+    }
+    @media screen and (max-width: 1600px) {
+        .financial .container-fluid {
+            padding-left: 15px;
+        }
+    }
+    @media screen and (min-width: 1601px) {
+        .financial .container-fluid {
+            padding-left: 218px;
+        }
+    }
+
+    @media screen and (max-width: 699px) {
+        .financial .container-fluid  {
+            padding: 15px;
+        }
+        
+        .financial .financial-title {
+            font-size: 50px;
+            text-align: center;
+        }
+    }
+</style>
---
+++ src/components/InfoTable.vue
@@ -0,0 +1,69 @@
+<script setup>
+    import Infomation from '../../src/Information.json'
+
+    const dataTable = Infomation;
+ </script>
+
+<template>
+    <section class="py-5">
+        <div class="container">
+            <div class="row">
+                <div class="col-12 text-center">
+                    <h2 class="info-title">Innovated Products That Gives You all <br> The <span><img src="/src/assets/Exprerian/InformationWording – 1.png" alt=""></span> You Need</h2>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-12">
+                    <div class="info-table">
+                        <div class="row">
+                            <div class="col-md-2 headings">Product</div>
+                            <div class="col-md-2 headings">Feature #1</div>
+                            <div class="col-md-2 headings">Feature #2</div>
+                            <div class="col-md-2 headings">Feature #3</div>
+                            <div class="col-md-2 headings">Feature #4</div>
+                            <div class="col-md-2 headings">Feature #5</div>
+                        </div>
+                        <div class="row" v-for="data in dataTable" :key="data">
+                            <div class="col-md-2 fields"> {{data.product}}</div>
+                            <div class="col-md-2 fields text-center"><i v-if="data.feature1 === 'x'" class="bi bi-check-circle-fill check-icon"></i></div>
+                            <div class="col-md-2 fields text-center"><i v-if="data.feature2 === 'x'" class="bi bi-check-circle-fill check-icon"></i></div>
+                            <div class="col-md-2 fields text-center"><i v-if="data.feature3 === 'x'" class="bi bi-check-circle-fill check-icon"></i></div>
+                            <div class="col-md-2 fields text-center"><i v-if="data.feature4 === 'x'" class="bi bi-check-circle-fill check-icon"></i></div>
+                            <div class="col-md-2 fields text-center"><i v-if="data.feature5 === 'x'" class="bi bi-check-circle-fill check-icon"></i></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</template>
+
+<style>
+    .info-title {
+        font-size: 50px;
+        font-weight: bold;
+        color: #1D4F91;
+    }
+    .headings {
+         padding-top: 10px;
+         padding-bottom: 10px;
+         border: 1px solid #6D2077;
+         background-color: #F0E9F1;
+     }
+     .fields {
+         padding-top: 10px;
+         padding-bottom: 10px;
+         border: 1px solid #6D2077;
+         justify-content: center;
+         background-color: #F0E9F1;
+         color: #333333;
+    
+     }
+    .fields:first-child {
+        font-weight: bold;
+    }
+    .check-icon {
+        color: green;
+        background-color:#F0E9F1;
+    }
+</style>
