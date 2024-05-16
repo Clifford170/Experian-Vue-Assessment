a48156b8baae81427193014268f2ad0f85ac0891
Clifford170 <cliffordbright107@gmail.com>
2024-05-16 09:23:23 +0200

    packages

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup>
+    import LandingPage from "./components/LandingPage.vue";
+    import LandingPageFooter from "./components/LandingPageFooter.vue"
+    import FinancialInsights from "./components/FinancialInsights.vue"
+    import Slider from "./components/Slider.vue"
+    import InfoTable from "./components/InfoTable.vue"
+    import SignupNow from "./components/SignupNow.vue"
+    import WeAreProud from "./components/WeAreProud.vue"
+    import bottomFooter from "./components/bottomFooter.vue"
+
+</script>
+<template>
+    <LandingPage/>
+    <LandingPageFooter />
+    <FinancialInsights />
+    <Slider />
+    <InfoTable />
+    <SignupNow />
+    <WeAreProud />
+    <bottomFooter />
+</template>
---
+++ src/main.js
@@ -0,0 +1,8 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import bootstrap from 'bootstrap/dist/js/bootstrap.bundle.js';
+import 'bootstrap-icons/font/bootstrap-icons.css'
+import './style.css'
+
+createApp(App).mount('#app')
